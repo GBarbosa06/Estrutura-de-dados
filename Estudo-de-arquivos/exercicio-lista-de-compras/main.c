c9a33b55a5edff34854c7b2de32e8613df391193
GBarbosa <103224622+GBarbosa06@users.noreply.github.com>
2025-02-26 11:13:21 -0300

    exercicio

---
+++ Estudo-de-arquivos/exercicio-lista-de-compras/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lerLista(){
+    FILE *f;
+    f = fopen("lista.csv", "r");
+    if (f==NULL)
+    {
+        printf("Erro");
+        exit(1);
+    }
+
+    char lixo[100];
+    fscanf(f, "%s", &lixo);
+
+    typedef struct
+    {
+        char nome[20];
+        int qtd;
+        float valor;        
+    }compra;
+
+    compra item[4];
+    float valorFinal = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        fscanf(f, "%[^,],%d,%f", &item[i].nome, &item[i].qtd, &item[i].valor); // [^,] --> para ler ate encontrar virgula
+        printf("%s: %d * %.2f = %.2f\n", item[i].nome, item[i].qtd, item[i].valor, item[i].qtd*item[i].valor);
+    }
+    
+       
+    fclose(f);
+}
+
+int main() {
+    lerLista();
+
+    return 0;
+}
