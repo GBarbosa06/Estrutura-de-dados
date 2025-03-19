f589813cd925d14f9872aab1ad495f3c0fb06eb5
GBarbosa <103224622+GBarbosa06@users.noreply.github.com>
2025-03-19 09:02:16 -0300

    criação de N aleatorios

---
+++ sorting/ex001.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int randNums(int range){
+
+    clock_t t0 = clock();
+
+    //codigo cujo tempo sera medido
+    int *V = (int *)malloc (range * sizeof(int)); //alocação dinâmica em tempo de execução
+    srand(time(NULL));
+    for (int i = 0; i < range; i++)  V[i] = rand();
+    for (int i = 0; i < range; i++) printf("%d \t", V[i]%100);
+
+    clock_t tf = clock();
+
+    double TempoCPU = ((double)(tf - t0)) / CLOCKS_PER_SEC;
+    printf("Tempo de execução: %.3lf seg. \n", TempoCPU);
+    
+}
+
+
+int main() {
+    randNums(10000);
+}
