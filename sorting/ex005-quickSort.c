7791c41422f56d347f07eec08edebf758e0fb92d
GBarbosa <andradegui2006@gmail.com>
2025-04-02 12:07:48 -0300

    correção

--- sorting/ex005-quickSort.c
+++ sorting/ex005-quickSort.c
@@ -33,7 +33,7 @@ if (esq < dir) {
 }
 
 int main() {
-    int arr[20] = {25, 17, 31, 13, 2, 19, 8, 14, 7, 1, 22, 10, 3, 12, 9, 5, 6, 16, 11, 4};
+    int arr[] = {25, 17, 31, 13, 2, 19, 8, 14, 7, 1, 22, 10, 3, 12, 9, 5, 6, 16, 11, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
     printf("Vetor desordenado:\n");
     for (int i = 0; i < n; i++) printf("%d ", arr[i]);
