f34697dab34060f29cf674b604d4ac67c57ce1d2
GBarbosa <103224622+GBarbosa06@users.noreply.github.com>
2025-04-16 09:39:26 -0300

    Criação da lista

--- Listas/Lista encadeadada/ex001.c
+++ Listas/Lista encadeadada/ex001.c
@@ -46,34 +46,45 @@ void inserirItem(Lista *l, Item i) {
     l->Tamanho++;
 }
 
-
 int main () {
-
 
     Item * E1 = criarItem(15);
     Item * E2 = criarItem(47);
     Item * E3 = criarItem(25);
     Item * E4 = criarItem(31);
 
+    Lista * L = criarLista();
+    inserirItem(L, *E1);
+    inserirItem(L, *E2);
+    inserirItem(L, *E3);
+    inserirItem(L, *E4);
+
+    printf("Tamanho da lista = %d\n", L->Tamanho);
+    printf("Endereco de L = %p\n", L);
+
+    printf("\n");
+
     printf("Endereco de E1 = %p\n", E1);
     printf("Endereco de E2 = %p\n", E2);
     printf("Endereco de E3 = %p\n", E3);
     printf("Endereco de E4 = %p\n", E4);
 
-    /* E1->prox = E2;
+     E1->prox = E2;
     E2->prox = E3;
     E3->prox = E4;
     E4->prox = NULL;
-
-    Item * E = E2;
-    printf("%d\n", E->chave);
-    Item * aux = E->prox;
-    printf("%d\n", aux->chave); */
+    /*
+     *   Item * E = E2;
+     *   printf("%d\n", E->chave);
+     *   Item * aux = E->prox;
+     *   printf("%d\n", aux->chave); 
+     */
 
     free(E1);
     free(E2);
     free(E3);
     free(E4);
+    free(L);
 
     return 0;
 }
