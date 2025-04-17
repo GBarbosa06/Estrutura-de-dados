6611a09f6c0101b88831401fb690a47389004dce
GBarbosa <andradegui2006@gmail.com>
2025-04-17 16:35:06 -0300

    Update cidades.c

---
+++ Trabalho individual - Estrada de Wakanda/cidades.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cidades.h"
+
+Estrada *getEstrada(const char *nomeArquivo){
+
+}             // Inicializa cidades no TAD indicado acima. 
+double calcularMenorVizinhanca(const char *nomeArquivo){
+
+}    // Retorna a menor vizinhança. 
+char *cidadeMenorVizinhanca(const char *nomeArquivo){
+    
+} 
