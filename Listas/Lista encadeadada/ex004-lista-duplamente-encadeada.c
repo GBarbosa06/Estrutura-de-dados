7c3dc026feaa3ef4be9eb6792b3b9dc14bca17d9
GBarbosa <andradegui2006@gmail.com>
2025-05-01 17:55:15 -0300

    Update ex004-lista-duplamente-encadeada.c

--- Listas/Lista encadeadada/ex004-lista-duplamente-encadeada.c
+++ Listas/Lista encadeadada/ex004-lista-duplamente-encadeada.c
@@ -7,3 +7,79 @@ typedef struct Node {
     struct Node* next;
     struct Node* prev;
 } Node;
+
+typedef struct DoublyLinkedList {
+    Node* head;
+    Node* tail;
+} DoublyLinkedList;
+
+DoublyLinkedList* createList() {
+    DoublyLinkedList* list = (DoublyLinkedList*)malloc(sizeof(DoublyLinkedList));
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
+
+void insertAtEnd(DoublyLinkedList* list, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = list->tail;
+
+    if (list->tail != NULL) {
+        list->tail->next = newNode;
+    } else {
+        list->head = newNode; // List was empty
+    }
+    list->tail = newNode;
+}
+
+void insertAtBeginning(DoublyLinkedList* list, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = list->head;
+    newNode->prev = NULL;
+
+    if (list->head != NULL) {
+        list->head->prev = newNode;
+    } else {
+        list->tail = newNode; // List was empty
+    }
+    list->head = newNode;
+}
+
+void deleteNode(DoublyLinkedList* list, Node* node) {
+    if (node->prev != NULL) {
+        node->prev->next = node->next;
+    } else {
+        list->head = node->next; // Node was head
+    }
+
+    if (node->next != NULL) {
+        node->next->prev = node->prev;
+    } else {
+        list->tail = node->prev; // Node was tail
+    }
+
+    free(node);
+}
+
+void printList(DoublyLinkedList* list) {
+    Node* current = list->head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void freeList(DoublyLinkedList* list) {
+    Node* current = list->head;
+    while (current != NULL) {
+        Node* next = current->next;
+        free(current);
+        current = next;
+    }
+    free(list);
+}
+
